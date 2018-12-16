4db1ef686d38b6a0be9906bb78ffa99ad9260e80
bayuirfan52 <bayuirfan52@gmail.com>
2018-12-16 17:57:40 +0700

    Fix Ball Function

--- app/src/main/java/com/kelas_a/pbo/pbo/fragment/Bola.java
+++ app/src/main/java/com/kelas_a/pbo/pbo/fragment/Bola.java
@@ -7,15 +7,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.kelas_a.pbo.pbo.R;
 import com.kelas_a.pbo.pbo.classes.Ball;
-import com.kelas_a.pbo.pbo.classes.Cube;
-import com.kelas_a.pbo.pbo.classes.Shape;
 
 public class Bola extends Fragment {
-    Shape bola;
-
+    Ball bola;
+    EditText jariJari;
+    Button hitung;
+    TextView volume, luas;
+    double jari, vol, luasBola;
 
     public Bola() {
         // Required empty public constructor
@@ -23,7 +27,7 @@ public class Bola extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bola, container, false);
@@ -32,8 +36,25 @@ public class Bola extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        bola = new Ball();
+        jariJari = view.findViewById(R.id.input_jariBola);
+        hitung = view.findViewById(R.id.btn_hitungBola);
+        volume = view.findViewById(R.id.VolumeBola);
+        luas = view.findViewById(R.id.LuasBola);
 
+        hitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jari = Double.valueOf(jariJari.getText().toString());
+
+                bola = new Ball(jari);
+
+                vol = bola.getVolume();
+                luasBola = bola.getLuas();
+
+                volume.setText(String.valueOf(vol));
+                luas.setText(String.valueOf(luasBola));
+            }
+        });
     }
 
 }
