5d44778a0a48559b33b36f42b9024667a76dff98
bayuirfan52 <bayuirfan52@gmail.com>
2018-12-16 14:20:09 +0700

    Make count activity

---
+++ app/src/main/java/com/kelas_a/pbo/pbo/activity/PerhitunganActivity.java
@@ -0,0 +1,15 @@
+package com.kelas_a.pbo.pbo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kelas_a.pbo.pbo.R;
+
+public class PerhitunganActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perhitungan);
+    }
+}
