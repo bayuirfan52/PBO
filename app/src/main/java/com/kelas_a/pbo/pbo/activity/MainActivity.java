b57c212b690833406bb562bded8f89eb1b746100
bayuirfan52 <bayuirfan52@gmail.com>
2018-12-16 11:27:36 +0700

    Menambahkan Fragment

---
+++ app/src/main/java/com/kelas_a/pbo/pbo/activity/MainActivity.java
@@ -0,0 +1,49 @@
+package com.kelas_a.pbo.pbo.activity;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.kelas_a.pbo.pbo.R;
+import com.kelas_a.pbo.pbo.fragment.DuaDimensiFragment;
+import com.kelas_a.pbo.pbo.fragment.TigaDimensiFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.duaDimensi :
+                        setFragment(new DuaDimensiFragment(),savedInstanceState);
+                        break;
+                    case R.id.tigaDimensi :
+                        setFragment(new TigaDimensiFragment(),savedInstanceState);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void setFragment(Fragment fragment, Bundle savedInstanceState){
+        if (savedInstanceState == null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frame_layout,fragment)
+                    .commit();
+        }
+    }
+}
---
+++ app/src/main/java/com/kelas_a/pbo/pbo/fragment/DuaDimensiFragment.java
@@ -0,0 +1,30 @@
+package com.kelas_a.pbo.pbo.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kelas_a.pbo.pbo.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DuaDimensiFragment extends Fragment {
+
+
+    public DuaDimensiFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dua_dimensi, container, false);
+    }
+
+}
